cd85b18638d0492129527ae1f01b0642f36b041a
Mo Wang <129330534+OneMore1@users.noreply.github.com>
2026-01-29 18:41:43 +0800

    Delete baselines directory

--- baselines/BrainCNNGNN/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility functions for training and evaluation.
-"""
-
-from .logger import setup_logger
-from .metrics import compute_metrics
-from .visualization import visualize_results
-
-__all__ = ['setup_logger', 'compute_metrics', 'visualize_results']
